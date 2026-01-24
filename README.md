07ea71b1e07e4eda148a251de1b64e17dda5cae5
Snxhit <amitdgames@gmail.com>
2026-01-24 10:23:11 +0530

    v0.0.3 - SQLite db, authentication - Add database compatibility so state persists - More objects - Add a function to handle disconnects/crashes safely

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Forlorn MUD
-## v0.0.2 (Major version 0: Under development)
+## v0.0.3 (Major version 0: Under development)
 A simple MUD engine written in the popular Go programming language.
 
 I have never used Golang before, please do not expect quality.
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"database/sql"
+	"fmt"
 	"net"
 	"os"
-	"fmt"
-	"bytes"
 	"strings"
+
+	_ "modernc.org/sqlite"
 )
 
 // networking
@@ -15,33 +18,71 @@ const (
 	CONN_TYPE = "tcp"
 )
 
+// stats
+var (
+	TotalConnections int = 0
+	TotalPlayers     int = 0
+)
+
 // state variables
-var ConnectionStore []ConnectionData = []ConnectionData{}
 
 // type structs
 type World struct {
-
+	characters []Character
 }
 
 type ConnectionData struct {
-	store net.Conn
+	ConnID  int
+	store   net.Conn
 	session *Session
 }
 
 type Session struct {
-	authorized *bool
-	id *int
-	username *string
-	password *string
-	character *Character
+	authorized bool
+	id         int
+	username   string
+	password   string
+	loginctx   *LoginContext
+	character  *Character
+}
+
+type LoginContext struct {
+	newPlayer bool
+	id        int
+	username  string
+	password  string
+	hp        int
 }
 
 type Character struct {
-	hp int
+	worldID   int
+	hp        int
 	inventory []string
 }
 
 func main() {
+	db, err := sql.Open("sqlite", "game.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	db.Exec(`
+		CREATE TABLE IF NOT EXISTS players (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			username TEXT UNIQUE NOT NULL,
+			password TEXT NOT NULL,
+			hp INT
+		)
+	`)
+
+	db.Exec(
+		"INSERT OR IGNORE INTO players(username, password, hp) VALUES (?, ?, ?)",
+		"testplayer", "hello", 100,
+	)
+
+	world := World{[]Character{}}
+
 	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
 	if err != nil {
 		fmt.Println("Error listening: ", err.Error())
@@ -52,25 +93,21 @@ func main() {
 	fmt.Println("Listening successfuly on: " + CONN_HOST + ":" + CONN_PORT)
 
 	for {
-		new_conn, err := l.Accept()
+		newConn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
 
-		FieldOne := false
-		FieldTwo := 0
-		FieldThree := ""
-		FieldFour := ""
-		sesh := Session{&FieldOne, &FieldTwo, &FieldThree, &FieldFour, nil}
-		conn := ConnectionData{new_conn, &sesh}
-		ConnectionStore = append(ConnectionStore, conn)
+		sesh := Session{false, 0, "", "", &LoginContext{false, 0, "", "", 100}, nil}
+		conn := ConnectionData{TotalConnections, newConn, &sesh}
+		TotalConnections += 1
 		fmt.Println(conn)
-		go HandleNewClient(&conn)
+		go HandleNewClient(&conn, &world, db)
 	}
 }
 
-func HandleNewClient(connection *ConnectionData) {
+func HandleNewClient(connection *ConnectionData, world *World, db *sql.DB) {
 	stream := connection.store
 	stream.Write([]byte("What is your name?\n> "))
 	for {
@@ -80,43 +117,84 @@ func HandleNewClient(connection *ConnectionData) {
 			fmt.Println("Error reading input: ", err.Error())
 		}
 
-		buf_cmd := bytes.Trim(buf, string([]byte{0}))
-		str_cmd := strings.ToLower(string(buf_cmd[0:len(buf_cmd) - 1]))
-		cmd_tokens := strings.Split(str_cmd, " ")
+		bufCmd := bytes.Trim(buf, string([]byte{0}))
+		// if the user interrupts
+		if len(bufCmd) == 0 {
+			HandleClientDisconnect(connection, world, db)
+			return
+		}
+		strCmd := strings.ToLower(string(bufCmd[0 : len(bufCmd)-1]))
+		cmdTokens := strings.Split(strCmd, " ")
 
-		if !*connection.session.authorized {
-			print()
-			if len(cmd_tokens) != 1 && len(*connection.session.username) == 0 {
-				stream.Write([]byte("Names can't have spaces, try again!"))
-			} else if len(cmd_tokens) == 1 && len(*connection.session.username) == 0 {
-				*connection.session.username = cmd_tokens[0]
-				stream.Write([]byte("Is " + cmd_tokens[0] + " your name? Then, enter a password."))
-			} else if len(cmd_tokens) != 1 && len(*connection.session.username) != 0 {
-				stream.Write([]byte("Passwords can't have spaces, try again!"))
-			} else if len(cmd_tokens) == 1 && len(*connection.session.username) != 0 {
-				*connection.session.password = cmd_tokens[0]
-				*connection.session.authorized = true
-				stream.Write([]byte("Welcome to the MUD!"))
-				fmt.Println(*connection.session)
+		if !connection.session.authorized {
+			if len(cmdTokens) != 1 {
+				stream.Write([]byte("No inputs can have spaces!"))
+			} else if len(cmdTokens) == 1 && len(connection.session.username) == 0 {
+				row := db.QueryRow("SELECT id, username, password, hp FROM players WHERE username = ?", cmdTokens[0])
+
+				err := row.Scan(&connection.session.loginctx.id, &connection.session.loginctx.username, &connection.session.loginctx.password, &connection.session.loginctx.hp)
+				if err != nil {
+					stream.Write([]byte("User " + cmdTokens[0] + " not found, creating user.\n"))
+					stream.Write([]byte("Please enter a new password."))
+					connection.session.username = cmdTokens[0]
+					connection.session.loginctx.newPlayer = true
+				} else {
+					connection.session.loginctx.newPlayer = false
+					connection.session.username = cmdTokens[0]
+					stream.Write([]byte("User " + cmdTokens[0] + " found, what is your password?"))
+				}
+			} else if len(cmdTokens) == 1 && len(connection.session.username) != 0 {
+				fmt.Println(connection.session.loginctx.password)
+				fmt.Println(connection.session.loginctx.newPlayer)
+				if !connection.session.loginctx.newPlayer {
+					if cmdTokens[0] == connection.session.loginctx.password {
+						stream.Write([]byte("Correct password! Welcome, " + connection.session.username + "!\n"))
+						connection.session.password = connection.session.loginctx.password
+						connection.session.id = connection.session.loginctx.id
+						connection.session.authorized = true
+						TotalPlayers += 1
+						world.characters = append(world.characters, Character{len(world.characters), connection.session.loginctx.hp, []string{""}})
+						fmt.Println(world.characters)
+						connection.session.character = &world.characters[len(world.characters)-1]
+						stream.Write([]byte("Welcome to the MUD!"))
+					} else {
+						stream.Write([]byte("Wrong password!"))
+					}
+				} else {
+					stream.Write([]byte("Welcome, " + connection.session.username + " to the game!\n"))
+					world.characters = append(world.characters, Character{len(world.characters), connection.session.loginctx.hp, []string{""}})
+					fmt.Println(world.characters)
+					connection.session.character = &world.characters[len(world.characters)-1]
+					prom, err := db.Exec("INSERT INTO players (username, password, hp) VALUES (?, ?, ?)", connection.session.username, cmdTokens[0], connection.session.character.hp)
+					fmt.Println(err)
+					id, _ := prom.LastInsertId()
+					connection.session.password = cmdTokens[0]
+					connection.session.id = int(id)
+					connection.session.authorized = true
+					TotalPlayers += 1
+					stream.Write([]byte("Welcome to the MUD!"))
+				}
 			}
 
 			stream.Write([]byte("\n> "))
 		} else {
-			switch len(cmd_tokens) {
+			switch len(cmdTokens) {
 			case 1:
-				switch cmd_tokens[0] {
+				switch cmdTokens[0] {
 				case "exit":
-					stream.Write([]byte("Exiting game."))
-					stream.Close()
+					HandleClientDisconnect(connection, world, db)
 					return
+				case "selfharm":
+					connection.session.character.hp -= 10
+					fmt.Println(connection.session.character.hp)
 				default:
 					stream.Write([]byte("Command not found!"))
 				}
 
 			case 2:
-				switch cmd_tokens[0] {
+				switch cmdTokens[0] {
 				case "echo":
-					stream.Write([]byte(cmd_tokens[1]))
+					stream.Write([]byte(cmdTokens[1]))
 				default:
 					stream.Write([]byte("Command not found!"))
 				}
@@ -125,4 +203,19 @@ func HandleNewClient(connection *ConnectionData) {
 			stream.Write([]byte("\n> "))
 		}
 	}
+}
+
+func HandleClientDisconnect(connection *ConnectionData, world *World, db *sql.DB) {
+	connection.store.Write([]byte("Exiting game."))
+	connection.store.Close()
+	if connection.session.authorized {
+		TotalPlayers -= 1
+		fmt.Println(connection.session.character.hp)
+		_, err := db.Exec("UPDATE players SET hp = ? WHERE id = ?", connection.session.character.hp, connection.session.id)
+		fmt.Println(err)
+		world.characters[connection.session.character.worldID] = Character{connection.session.character.worldID, 100, []string{""}}
+		connection.session = &Session{false, 0, "", "", nil, nil}
+	}
+	TotalConnections -= 1
+	fmt.Println(world.characters)
 }
